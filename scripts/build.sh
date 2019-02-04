ba0bbd4fe581c2056bf110ec54e799ca2bbf322b
Jake Howard <git@theorangeone.net>
2019-02-04 13:46:15 +0000

    Add static pipeline

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf public/
+
+mkdir -p static/build/js static/build/css
+
+cp -r static/src/* static/build
+
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js static/build/js/bootstrap.js
+cp -r node_modules/@fortawesome/fontawesome-free/css/all.min.css static/build/css/font-awesome.css
+cp -r node_modules/@fortawesome/fontawesome-free/webfonts static/build
+
+hugo -v --stepAnalysis --gc
