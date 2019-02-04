6469d5d69e739aef6b55d66fc2a3b25c2540b86f
Jake Howard <git@theorangeone.net>
2019-02-04 13:29:59 +0000

    Just make it a static site, no lambda

--- lambda/index.ts
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-export function handler(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(fs.readdirSync(__dirname))
-  });
-}
