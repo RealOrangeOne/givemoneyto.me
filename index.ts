f9f4ae3460e5f7ecd34c60ad53dd89a6244c97c6
Jake Howard <git@theorangeone.net>
2019-06-29 17:21:43 +0100

    Add progress bar when outputting pages

--- index.ts
+++ index.ts
@@ -5,15 +5,21 @@ import { readFileSync, writeFileSync } from 'fs';
 import Handlebars from 'handlebars';
 import mkdirp from 'mkdirp';
 import { range } from 'underscore';
+import ProgressBar from 'progress';
 
 const BUILD_DIR = join(__dirname, 'build');
 const SRC_DIR = join(__dirname, 'src');
+const PROGRESS_BAR_FORMAT = "[:bar] :rate/ps :percent";
 
 const BUNDLER_OPTIONS = {
   outDir: BUILD_DIR,
   watch: false,
   minify: true,
 };
+
+function statusOutput(message: string) {
+  console.log("> " + message + "...");
+}
 
 function writeTemplate(content: string, value: string) {
   const outputFile = join(BUILD_DIR, value, 'index.html');
@@ -29,14 +35,22 @@ function renderTemplate(template: HandlebarsTemplateDelegate, value: number) {
 (async function() {
   rimraf.sync(BUILD_DIR);
 
+  statusOutput("Creating template");
   const bundler = new Bundler(join(SRC_DIR, 'template.html'), BUNDLER_OPTIONS);
-
   await bundler.bundle();
 
+  statusOutput("Compiling HTML template");
   const template = Handlebars.compile(readFileSync(join(BUILD_DIR, 'template.html')).toString());
 
-  range(0, 10, 0.5).forEach((i) => {
-    console.log(i);
+  const possibleValues = range(0, 10, 0.5);
+  const bar = new ProgressBar(PROGRESS_BAR_FORMAT, {
+    total: possibleValues.length,
+    width: 40
+  });
+
+  statusOutput("Generating pages");
+  possibleValues.forEach((i) => {
     renderTemplate(template, i);
+    bar.tick();
   });
 })();
