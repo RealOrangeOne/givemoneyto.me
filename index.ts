b8b16f3ba06a476cb53d9bfd852882338c96ac75
Jake Howard <git@theorangeone.net>
2019-06-29 17:43:03 +0100

    Install (and run) prettier

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import ProgressBar from 'progress';
 
 const BUILD_DIR = join(__dirname, 'build');
 const SRC_DIR = join(__dirname, 'src');
-const PROGRESS_BAR_FORMAT = "[:bar] :rate/ps :percent :current/:total";
+const PROGRESS_BAR_FORMAT = '[:bar] :rate/ps :percent :current/:total';
 const MAX_VALUE = process.env.MAX_VALUE ? parseInt(process.env.MAX_VALUE) : 10;
 
 const BUNDLER_OPTIONS = {
@@ -19,7 +19,7 @@ const BUNDLER_OPTIONS = {
 };
 
 function statusOutput(message: string) {
-  console.log("> " + message + "...");
+  console.log('> ' + message + '...');
 }
 
 function isPrecision(value: number, precision: number) {
@@ -33,35 +33,37 @@ function writeTemplate(content: string, value: string) {
 }
 
 function renderTemplate(template: HandlebarsTemplateDelegate, value: number) {
-  const html = template({value});
+  const html = template({ value });
   writeTemplate(html, value.toString());
   if (isPrecision(value, 1)) {
-    writeTemplate(html, value.toString() + "0");
+    writeTemplate(html, value.toString() + '0');
   }
   if (isPrecision(value, 0)) {
-    writeTemplate(html, value.toString() + ".0");
-    writeTemplate(html, value.toString() + ".00");
+    writeTemplate(html, value.toString() + '.0');
+    writeTemplate(html, value.toString() + '.00');
   }
 }
 
 (async function() {
   rimraf.sync(BUILD_DIR);
 
-  statusOutput("Creating template");
+  statusOutput('Creating template');
   const bundler = new Bundler(join(SRC_DIR, 'template.html'), BUNDLER_OPTIONS);
   await bundler.bundle();
 
-  statusOutput("Compiling HTML template");
-  const template = Handlebars.compile(readFileSync(join(BUILD_DIR, 'template.html')).toString());
+  statusOutput('Compiling HTML template');
+  const template = Handlebars.compile(
+    readFileSync(join(BUILD_DIR, 'template.html')).toString()
+  );
 
   const possibleValues = range(0, MAX_VALUE, 0.01);
   const bar = new ProgressBar(PROGRESS_BAR_FORMAT, {
     total: possibleValues.length,
-    width: 40
+    width: 40,
   });
 
-  statusOutput("Generating pages");
-  possibleValues.forEach((i) => {
+  statusOutput('Generating pages');
+  possibleValues.forEach(i => {
     if (i) {
       renderTemplate(template, parseFloat(i.toFixed(2)));
     }
